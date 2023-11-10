16ee8eac15482ddedf8e5dc9b25a58a64daaa40a
liunan <2890853803@qq.com>
2023-11-10 12:41:09 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 关闭语法检查
 Vue.config.productionTip = false
+
+// 引入默认样式
+import './assets/reset.css'
+
+// 注册全局组件
+import Header from './components/header/Header.vue'
+import Footer from './components/footer/Footer.vue'
+Vue.component('Header', Header)
+Vue.component('Footer', Footer)
+
 
 new Vue({
   render: h => h(App),
